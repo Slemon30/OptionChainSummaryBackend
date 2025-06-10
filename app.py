9d269f578f6be489b6b1c9e44f34d12565a5a74b
aryavsingla7 <116359284+Slemon30@users.noreply.github.com>
2025-06-10 12:45:07 +0530

    Added CRON Job

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ from flask_cors import CORS
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+# import logging
+
+# logging.basicConfig(level=logging.DEBUG,
+#     format='%(asctime)s %(levelname)s [%(name)s] %(message)s')
+# logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+cached_data = {
+    "option_chain": [],
+    "summary": {}
+}
 
 app = Flask(__name__)
 CORS(app)
@@ -11,32 +23,29 @@ def fetch_option_data():
     optiondata_list = []
     url = 'https://groww.in/options/nifty'
 
-    # Set headers to mimic a browser request
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
     }
 
-    # Make the request
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find all tables with the specified classes
     tables = soup.find_all('table', class_='tb10Table borderPrimary optc56Table')
 
     if tables:
-    # Select the first table in the list
+   
         table = tables[0]
     
-    # Find the tbody within this table
+ 
         tbody = table.find('tbody', class_='optc56Tbody optc56TbodyOv')
     
         if tbody:
-        # Find all tr tags within the tbody
+       
             rows = tbody.find_all('tr')
         
-        # Process each tr tag
+      
             for row in rows:
-            # For each tr, find all td tags
+            
                 CallcellsOI = row.find_all('td', class_='opr84CallCell', attrs={"width":"14.5%"})
                 CallcellsPrice = row.find_all('td', class_='opr84CallCell', attrs={"width":"14%"})
                 StrikePrice = row.find_all('td', class_= 'tb10Td')
@@ -46,26 +55,26 @@ def fetch_option_data():
             
                 for td in CallcellsOI:
                     callcelldiv_OI = td.find('div', class_='opr84CellVal')
-                    callOI = callcelldiv_OI.text.replace(",", "").strip()  # Get the text from the div
+                    callOI = callcelldiv_OI.text.replace(",", "").strip()  
                     if callOI == '--':
                         callOI = 0
                     else:
                         callOI = float(callOI)
 
-                #print("Call OI: ", callOI)  # Print the Call OI value
+                #print("Call OI: ", callOI)  
                 for td in CallcellsPrice:
                     callcelldiv_Price = td.find('div', class_='opr84CellVal')
-                    callPrice = callcelldiv_Price.text.replace(",", "").replace("₹", "").strip()  # Get the text from the div
+                    callPrice = callcelldiv_Price.text.replace(",", "").replace("₹", "").strip()  
                     if callPrice == '--':
                         callPrice = 0
                     else:
                         callPrice = float(callPrice)
-                #print("Call Price: ", callPrice)  # Print the Call OI value
+                #print("Call Price: ", callPrice)  
 
                 for td in StrikePrice:
                     celldiv_Price = td.find('span', class_='opr84AbsoluteCentre')
-                    strikePrice = float(celldiv_Price.text.replace(",", "").strip())  # Get the text from the div
-                #print("Strike Price: ", strikePrice)  # Print the Call OI value
+                    strikePrice = float(celldiv_Price.text.replace(",", "").strip()) 
+                #print("Strike Price: ", strikePrice)  
                 for td in PutcellsOI:
                     putcelldiv_OI = td.find('div', class_='opr84CellVal')
                     putOI = putcelldiv_OI.text.replace(",", "").strip()
@@ -73,16 +82,16 @@ def fetch_option_data():
                         putOI = 0
                     else:
                         putOI = float(putOI)
-                #print("Put OI: ", putOI)  # Print the Call OI value
+                #print("Put OI: ", putOI)  
 
                 for td in PutcellsPrice:
                     putcelldiv_Price = td.find('div', class_='opr84CellVal')
-                    putPrice = putcelldiv_Price.text.replace(",", "").replace("₹", "").strip()  # Get the text from the div
+                    putPrice = putcelldiv_Price.text.replace(",", "").replace("₹", "").strip()  
                     if putPrice == '--':
                         putPrice = 0
                     else:
                         putPrice = float(putPrice)
-                #print("Put Price: ", putPrice)  # Print the Call OI value
+                #print("Put Price: ", putPrice)  
                 callplusputOI = callOI + putOI
                 callminusputOI = callOI - putOI
                 data1 = [callOI, callPrice, strikePrice, putPrice, putOI, callplusputOI, callminusputOI]
@@ -95,8 +104,7 @@ def fetch_option_data():
 
     return optiondata_list
 
-@app.route('/get_option_data', methods=['GET'])
-def get_option_data():
+def update_data():
     optiondata_list = fetch_option_data()
     optionchain = pd.DataFrame(optiondata_list, columns=['Call OI', 'Call Price', 'Strike Price', 'Put Price', 'Put OI', 'Call+Put', 'Call-Put'])
     
@@ -133,10 +141,8 @@ def get_option_data():
     #else:
         #print("No Strike Price found with maximum combined OI.")
 
-    
-    return jsonify({
-        "option_chain": optiondata_list,
-        "summary": {
+    cached_data["option_chain"] = optiondata_list
+    cached_data["summary"] = {
             "totalCallOI": totalCallOI,
             "totalPutOI": totalPutOI,
             "putToCallRatio": put_to_call_ratio,
@@ -145,9 +151,17 @@ def get_option_data():
             "maxOI_StrikePrice": maxOI_StrikePrice,
             "maxOI_StrikePrice2": maxOI_StrikePrice2,
             "maxOI_StrikePrice3": maxOI_StrikePrice3
-            
         }
-    })
 
-if __name__ == '__main__':
+@app.route('/get_option_data', methods=['GET'])
+def get_option_data():
+    return jsonify(cached_data)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_data, "interval", minutes=1)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
+
+if __name__ == "__main__":
+    update_data()
     app.run(debug=True)
